1bceabeb75e8c74a4a01c1f4899e5fe48021aca5
HajeraSultana <e031306@gmail.com>
2024-09-12 08:18:08 +0100

    Create day18.py

---
+++ day18.py
@@ -0,0 +1,27 @@
+print("+++Number gussing Challenge+++")
+print()
+print("Guess the number between 1 and 1,000,000 and I will tell you if you are too low, too high or get it correct.")
+print()
+print("Let's play!")
+print()
+correct_number = 2300
+attempt = 1
+while True:
+  user_guess = int(input("Pick a number between 1 and 1,000.000:"))
+  if user_guess < 0:
+    print("Now we'll never know what the answer is ...")
+    exit()
+  if user_guess < correct_number:
+    print("That number is too low. Try again!")
+    attempt += 1
+    print()
+  elif user_guess > correct_number:
+    print("That number is too high. Try again!")
+    attempt += 1
+    print()
+    continue
+  elif user_guess == correct_number:
+    print ("It took you", attempt, "shots to hit the mark!")
+    break
+  else:
+    print("That is not a number I recognize.")
